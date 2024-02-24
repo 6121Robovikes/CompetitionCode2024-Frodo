d70d454b259b06d5a43aef4592d44915e2340fd7
6121 Robovikes <35512043+6121Robovikes@users.noreply.github.com>
2024-02-24 10:43:55 -0500

    pivot

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -33,10 +33,12 @@ public class PivotSubsystem extends SubsystemBase {
      // Motion Profile Position
     var slot0Configs = new Slot0Configs();
     slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
-    slot0Configs.kV = 0.12; //A velocity target
-    slot0Configs.kP = 4.8; 
-    slot0Configs.kI = 0; 
-    slot0Configs.kD = 0.1; 
+    slot0Configs.kV = 0.12; //A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kP = 4.8; //A position error of 2.5 rotations results in 12 V output
+    slot0Configs.kI = 0; //no output for integrated error
+    slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+    m_pivotMotor.getConfigurator().apply(slot0Configs);
   }
 
   @Override
